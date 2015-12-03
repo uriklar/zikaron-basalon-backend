6a95bb7b856b7986a71f8f5fd14474891ea7e789
Uri Klar <uriklar@gmail.com>
2015-12-03 10:00:49 +0700

    request model validations

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,8 @@
 class Request < ActiveRecord::Base
+	belongs_to :host
+	belongs_to :guest
+
+	validates :host_id, presence: true
+	validates :guest_id, presence: true
+	validates :host_id, uniqueness: {scope: :guest_id}
 end
