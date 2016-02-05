041ae0f7dd0106d69865c9ae9af0bc535c9b3bf8
Uri Klar <uri.k@phytech.com>
2016-02-05 10:17:57 +0200

    fixes seed

--- app/models/host.rb
+++ app/models/host.rb
@@ -2,7 +2,6 @@ class Host < ActiveRecord::Base
 	has_one :user, as: :meta, dependent: :destroy
 	belongs_to :country
 	belongs_to :city
-	accepts_nested_attributes_for :user
 	has_many :requests
 
   def city=(val)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,105 +5,107 @@
 #
 #   cities = City.create([{ name=> 'Chicago' }, { name=> 'Copenhagen' }])
 #   Mayor.create
+
 User.destroy_all
 users = User.create([
-    {
-        email: "admin@mailinator.com",
-        password: "1234.com",
-        password_confirmation: "1234.com",
-        admin: true
-    }
+	{
+		email: "admin@mailinator.com",
+		password: "1234.com",
+		password_confirmation: "1234.com",
+		admin: true
+	},
+	{
+		email: "user1@mailinator.com",
+		password: "1234.com",
+		password_confirmation: "1234.com",
+		phone: "0527738447",
+		first_name: "User1",
+		last_name: "User1"
+	},
+	{ 
+		email: "user2@mailinator.com",
+		password: "1234.com",
+		password_confirmation: "1234.com",
+		phone: "0523112137",
+		first_name: "User2",
+		last_name: "User2"
+	}
 ])
 
 Host.destroy_all
 hosts = Host.create([
-    {
-        "address"=> "איינשטיין 29, רעננה, ישראל",
-        "free_text"=> "אושפזה עם זיהום חריף יומיים לפני יום השואה ונאלצה לבטל",
-        "strangers_allowed"=> false,
-        "lat"=> 32.1839926,
-        "lng"=> 34.8830608000001,
-        "user_attributes" => 
-            { 
-                email: "user1@mailinator.com",
-                password: "1234.com",
-                password_confirmation: "1234.com",
-                phone: "0527738447",
-                first_name: "User1",
-                last_name: "User1"
-            }
-    },
-    {
-        
-        "address"=> "שלום עליכם, תל אביב יפו, ישראל",
-        "max_guests"=> 20,
-        "free_text"=> "קומה ראשונה - 4/5 מדרגות. היה מארח בעבר (כמה וכמה פעמים)",
-        "strangers_allowed"=> false,
-        "lat"=> 32.0770745,
-        "lng"=> 34.7695493,
-        "user_attributes" => 
-            { 
-                email: "user2@mailinator.com",
-                password: "1234.com",
-                password_confirmation: "1234.com",
-                phone: "0523112137",
-                first_name: "User2",
-                last_name: "User2"
-            }
-        
-    },
-    {
-        "address"=> "החרש 4, הוד השרון, ישראל",
-        "max_guests"=> 15,
-        "free_text"=> "אשמח לארח את תמי, תודה!",
-        "strangers_allowed"=> true,
-        "lat"=> 32.1324472,
-        "lng"=> 34.8987391000001,
-        "user_attributes" => 
-            { 
-                email: "user3@mailinator.com",
-                password: "1234.com",
-                password_confirmation: "1234.com",
-                phone: "0523155137",
-                first_name: "User3",
-                last_name: "User3"
-            }
-        
-    }
+	{
+		hosted_before: true,
+		org_name: "Boyscouts",
+		org_role: "Head Scout",
+		country: "ישראל",
+		city: "רעננה",
+		address: "איינשטיין 29",
+		lat: "32.1839926",
+		lng: "34.8830608000001",
+		floor: 2,
+		elevator: false,
+		stairs: true,
+		event_date: "4.5.2016",
+		event_time: "20:00",
+		language: "אנגלית",
+		survivor_needed: true,
+		public: true,
+		free_text: "I have a big dog",
+		user_id: users[1].id
+	},
+	{
+		hosted_before: false,
+		country: "ישראל",
+		city: "תל אביב יפו",
+		address: "שלום עליכם",
+		lat: "32.0770745",
+		lng: "34.7695493",
+		floor: 2,
+		elevator: false,
+		stairs: true,
+		event_date: "4.5.2016",
+		event_time: "20:00",
+		language: "עברית",
+		survivor_needed: true,
+		public: true,
+		free_text: "My grandma lives on my couch",
+		user_id: users[2].id
+	}
 ])
 
 Guest.destroy_all
 guests = Guest.create([
-    {
-        user_attributes: {
-            email: "guest1@mailinator.com",
-            phone: "12345",
-            first_name: "Guest1",
-            last_name: "Guest1"
-        }
-    },
-    {
-        user_attributes: {
-            email: "guest2@mailinator.com",
-            phone: "12345",
-            first_name: "Guest2",
-            last_name: "Guest2"
-        }
-    },
+	{
+		user_attributes: {
+			email: "guest1@mailinator.com",
+			phone: "12345",
+			first_name: "Guest1",
+			last_name: "Guest1"
+		}
+	},
+	{
+		user_attributes: {
+			email: "guest2@mailinator.com",
+			phone: "12345",
+			first_name: "Guest2",
+			last_name: "Guest2"
+		}
+	}
 ])
 
 Request.destroy_all
 requests = Request.create([
-    {
-        host_id: hosts[0].id,
-        guest_id: guests[0].id
-    },
-    {
-        host_id: hosts[0].id,
-        guest_id: guests[1].id
-    },
-    {
-        host_id: hosts[1].id,
-        guest_id: guests[0].id
-    }
-])
+	{
+		host_id: hosts[0].id,
+		guest_id: guests[0].id
+		},
+		{
+			host_id: hosts[0].id,
+			guest_id: guests[1].id
+			},
+			{
+				host_id: hosts[1].id,
+				guest_id: guests[0].id
+			}
+			])
